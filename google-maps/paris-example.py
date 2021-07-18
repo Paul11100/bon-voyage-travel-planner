5f26885f88c4e78da425a1a6b34e74a2280a0258
Xiaofeng Paul Lin <paullam11100@hotmail.com>
2021-07-18 14:58:37 +0100

    Add more places to paris example

--- google-maps/paris-example.py
+++ google-maps/paris-example.py
@@ -1,5 +1,8 @@
+'''Three Group of Paris Landmark'''
 import routes
 places = ['Panthéon, Paris', 'Eiffel Tower, Paris', 'Louvre Museum, Paris', 'Arc de Triomphe, Paris']
+# places = ['Cathédrale Notre-Dame de Paris', "Musée d'Orsay", 'Palais Garnier']
+# places = ['Sainte-Chapelle, Paris', 'Moulin Rouge, Paris', 'Les Invalides, Paris']
 path = routes.shortest_path(places, routes.map_client)
 map_url = routes.generate_map_url(path[0], path[len(path)-1], '|'.join(path[1:-1]))
 print(map_url)
